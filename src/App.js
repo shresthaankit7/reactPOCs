a9260d026b211601d57f3702dd7caa9a4d7800ad
shresthaankit7 <shresthaankit7@gmail.com>
2020-03-11 22:49:56 +0545

    Added Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Greet from './components/Greet';
+import Namaste from './components/Namaste';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +22,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Greet name="Rooney" heroName="Batman">
+          <button>Click Me</button>
+        </Greet>
+        <Greet name="Scholes" heroName="SuperMan"/>
+        <Namaste name="England"/>
+        <Namaste name="Nepal"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Namaste.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Namaste extends Component {
+    render(){
+        return <h1>Namaste {this.props.name}.</h1>
+    }
+}
+
+export default Namaste;
