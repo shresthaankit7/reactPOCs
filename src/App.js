183807f842e693e8049ac03532a294bd69ef1d57
shresthaankit7 <shresthaankit7@gmail.com>
2020-04-19 22:48:03 +0545

    React-Redux MAIN PARTs

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import Greet from './components/Greet';
-import Namaste from './components/Namaste';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import Form from './components/Forms/Form';
-import Table from './components/Fragments/Table';
-import EventBind from './components/EventBind';
-import FunctionClick from './components/FunctionClick';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import CakeContainer from './components/CakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-scroll">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-        <div>
-          {/* Greet Component */}
-          <div>
-            <Greet name="Rooney" heroName="Batman">
-              <button>Click Me</button>
-            </Greet>
-            <Greet name="Scholes" heroName="SuperMan" />
-          </div>
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer/>
+        <CakeContainer/>
 
-          {/* Namaste Component */}
-          <Namaste name="England" />
-          <Namaste name="Nepal" />
-
-          {/* Message Component */}
-          <Message />
-
-          {/*Testing Counters */}
-          <Counter />
-
-          { /*Function click*/}
-          <FunctionClick />
-
-          {/*Class Event Bind*/}
-          <EventBind />
-
-          {/* Form Component */}
-          <Form />
-
-        </div>
+        <IceCreamContainer/>
       </div>
-      <div>
-        <Table/>
-      </div>
-      <div>
-        <footer>
-          <p>Please don't push this to production</p>
-          <p>Demo in progress</p>
-        </footer>
-
-      </div>
-
-    </div>
+    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* React Part
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -13,61 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-*/
-
-
-/* Redux tutorials
-*/
-const redux = require('redux') 
-
-const createStore = redux.createStore
-const applyMiddleware = redux.applyMiddleware
-const thunkMiddleware = require('redux-thunk').default
-const axios = require('axios')
-
-const BUY_CAKE = 'BUY_CAKE'
-const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
-
-function buyCake(){
-    return {
-        type: BUY_CAKE,
-        info: 'First Action'
-    }
-}
-
-function buyIceCream(){
-    return {
-        type: BUY_ICE_CREAM,
-        info: 'Second Action'
-    }
-}
-
-const initialState = {
-    numOfCakes: 10,
-    numOfIcreCream: 20
-}
-
-const reducer = (state = initialState, action ) =>{
-    switch (action.type){
-        case BUY_CAKE: return{
-            ...state,
-            numOfCakes: state.numOfCakes-1
-        }
-        case BUY_ICE_CREAM: return{
-            ...state,
-            numOfIcreCream: state.numOfIcreCream-1
-        }
-        default: return state
-    }
-}
-
-const store = createStore(reducer, applyMiddleware(thunkMiddleware))
-console.log("initial state",store.getState());
-
-const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getState()))
-
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyIceCream())
-
-unsubscribe();
